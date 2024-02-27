380d4b94e0bd1235d0429486b281e8a94f094bc4
AniBots4 <161492552+AniBots4@users.noreply.github.com>
2024-02-27 22:01:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,14 +1,14 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "22407569"))
+  API_HASH = os.environ.get("API_HASH", "8f7fc33ab655b491b722e6474e0328c3")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7169866921:AAEH-XegcfJ02e_8fJv8laoPjLF75JKtgmk")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Animax_Bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", 6362118709""))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6125916003"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
